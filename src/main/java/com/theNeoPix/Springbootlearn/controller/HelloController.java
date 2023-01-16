324c526af5e52361f6859f02db3f7e1faf11ec03
Kishan-Kumar-Zalavadia <kishankumarz1048@gmail.com>
2023-01-16 11:12:42 +0530

    Added Devtools

--- src/main/java/com/theNeoPix/Springbootlearn/controller/HelloController.java
+++ src/main/java/com/theNeoPix/Springbootlearn/controller/HelloController.java
@@ -1,8 +1,6 @@
 package com.theNeoPix.Springbootlearn.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String helloPix(){
-        return "Welcome TheNeoPix";
+        return "Welcome TheNeoPix again!!";
     }
 }
