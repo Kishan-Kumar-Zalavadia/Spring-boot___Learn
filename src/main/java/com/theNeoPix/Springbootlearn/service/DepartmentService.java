cee381146e53697d4e3506cf97c54a216d85f0d4
Kishan-Kumar-Zalavadia <kishankumarz1048@gmail.com>
2023-01-16 13:37:39 +0530

    fetch all data

--- src/main/java/com/theNeoPix/Springbootlearn/service/DepartmentService.java
+++ src/main/java/com/theNeoPix/Springbootlearn/service/DepartmentService.java
@@ -2,6 +2,10 @@ package com.theNeoPix.Springbootlearn.service;
 
 import com.theNeoPix.Springbootlearn.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
 }
