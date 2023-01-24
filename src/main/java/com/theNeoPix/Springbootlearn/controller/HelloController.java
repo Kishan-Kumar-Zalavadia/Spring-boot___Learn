92d92bc8c5e65ff536bf3f0f37ea06efe522e409
Kishan-Kumar-Zalavadia <kishankumarz1048@gmail.com>
2023-01-24 09:59:17 +0530

    Adding config in properties file

--- src/main/java/com/theNeoPix/Springbootlearn/controller/HelloController.java
+++ src/main/java/com/theNeoPix/Springbootlearn/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.theNeoPix.Springbootlearn.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String helloPix(){
-        return "Welcome TheNeoPix again!!";
+        return welcomeMessage;
     }
 }
